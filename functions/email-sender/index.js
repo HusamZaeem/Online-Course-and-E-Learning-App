4ed52bf479ed8ce1e1c85e91c9c532a9e7d67e22
HusamZaeem <husamzaeem@gmail.com>
2024-12-28 16:45:53 +0100

    Updated Forgot Password

--- functions/email-sender/index.js
+++ functions/email-sender/index.js
@@ -38,7 +38,7 @@ app.post('/send-otp', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
